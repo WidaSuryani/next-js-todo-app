e0a9d9cea838c8a15a0ac5ca6f0a622d40dfaffa
Wida Suryani <widasuryani1@gmail.com>
2022-08-01 23:38:48 +0800

    feat : user can delete data

--- component/to-do-list.tsx
+++ component/to-do-list.tsx
@@ -42,7 +42,7 @@ const TodoList = () => {
 }
 
 const List = () => {
-  const { items, removeCompleted } = useStore()
+  const { items, removeItem } = useStore()
 
   return (
     <div>
@@ -51,10 +51,16 @@ const List = () => {
         .map(item => (
           <div className="flex mb-4 items-center">
             <p className="w-full text-grey-darkest">{item.title} </p>
-            <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">
+            <button
+              className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green done:line-through"
+              onClick={() => (item.done ? 'done' : 'line-through')}
+            >
               Done
             </button>
-            <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">
+            <button
+              className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red"
+              onClick={() => removeItem(item)}
+            >
               Remove
             </button>
           </div>
