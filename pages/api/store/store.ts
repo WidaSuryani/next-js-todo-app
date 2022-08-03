a37e2bbef90d5324799264ee54414aa035eaf96e
Wida Suryani <widasuryani1@gmail.com>
2022-08-03 17:42:01 +0800

    feat : add edit fuction edit [WIP]

--- pages/api/store/store.ts
+++ pages/api/store/store.ts
@@ -1,11 +1,11 @@
 import create from 'zustand';
 import { devtools } from 'zustand/middleware';
-import { persist } from 'zustand/middleware';
 
 export type TodoItem = {
   id: number;
   title: string;
   done: boolean;
+  edit: string;
 };
 
 export interface State {
@@ -13,6 +13,7 @@ export interface State {
   addItem(item: TodoItem): void;
   toggleItem(item: TodoItem): void;
   removeItem(item: TodoItem): void;
+  // editItem(item: TodoItem): void;
   removeCompleted(): void;
 }
 
@@ -21,6 +22,7 @@ export const useStore = create<State>(
       (set) => ({
         items: [],
         addItem: (item: TodoItem) => set((state) => ({ items: [...state.items, item] }), false, 'ADD_ITEM'),
+        // editItem: (item: TodoItem) => set((state) => ({ items: [...state.items, item] }), false, 'ADD_ITEM'),
         removeItem: (item: TodoItem) => set((state) => ({ items: state.items.filter((it) => it.id !== item.id) }), false, 'REMOVE_ITEM'),
         toggleItem: (item: TodoItem) =>
           set(
@@ -30,6 +32,14 @@ export const useStore = create<State>(
             false,
             'TOGGLE_ITEM'
           ),
+          // editItem: (item: TodoItem) =>
+          // set(
+          //   (state) => ({
+          //     items: state.items.map((it) => ({ ...it, edit: it.title === item.title ? !item.title : it.title }))
+          //   }),
+          //   false,
+          //   'EDIT_ITEM'
+          // ),
         removeCompleted: () => set((state) => ({ items: state.items.filter((it) => !it.done) }), false, 'REMOVE_COMPLETED')
       }),
       'TodoStore'
